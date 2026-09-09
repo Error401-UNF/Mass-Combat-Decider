9c98c56993f2ca6a1cec27012564e88bc8aa1c54
Aaron Brey (script) <aabrey321@gmail.com>
2026-09-09 14:27:05 -0400

    chore: update version number

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     # 1. The core Rust package (built with standard rustPlatform)
     massCombatDecider = pkgs.rustPlatform.buildRustPackage {
       pname = "MassCombatDecider";
-      version = "0.2.2";
+      version = "0.2.3";
       src = self; 
       cargoLock = { lockFile = ./Cargo.lock; };
       nativeBuildInputs = with pkgs; [ pkg-config ];
@@ -28,7 +28,7 @@
     # 2. The final bundled package (creates the AppImage-like wrapper)
     bundledApp = pkgs.stdenv.mkDerivation {
       pname = "MassCombatDecider";
-      version = "0.2.2";
+      version = "0.2.3";
       src = massCombatDecider; 
       nativeBuildInputs = [ pkgs.makeWrapper ]; 
       buildInputs = gtkDeps;
